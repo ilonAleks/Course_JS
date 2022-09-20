759947913617d8f7fe59639278405f9544fd5577
ilonaleks <rogilona@gmail.com>
2022-09-20 12:14:24 +0300

    All_6_tasks

--- 6. Introduction to the basics of JavaScript/1/6-1.js
+++ 6. Introduction to the basics of JavaScript/1/6-1.js
@@ -4,20 +4,6 @@ const existedUserPassword = '12345678'
 let userLogin = prompt('Enter Login')
 let userPassword = prompt('Enter password')
 
-if (!userLogin) {
-	alert('You did not enter your login! Try again')
-	userLogin = prompt('Enter Login')
-} else if (!userPassword) {
-	alert('You did not enter your password! Try again')
-	userPassword = prompt('Enter password')
-}
-
-//! проверка работает не совсем корректно
-//! т.е. если например не ввести с первого раза ни логин, ни пароль,
-//! предложат ввести логин второй раз и все равно выведется сообщение, 
-//! что логин-пароль не совпадает. А надо, чтобы выводилось еще и предложение
-//! ввести пароль
-
 userLogin = userLogin.trim()
 userPassword = userPassword.trim()
 
--- 6. Introduction to the basics of JavaScript/6/6-6.js
+++ 6. Introduction to the basics of JavaScript/6/6-6.js
@@ -1,48 +1,30 @@
-// let clientName = prompt('Введите имя клиента')
+let clientName = prompt('Введите имя клиента')
 let clientSpentToday = prompt('Сколько клиент потратил сегодня?')
 let clientSpentForAllTime = prompt('Сколько клиент потратил за все время?')
 
-console.log(clientSpentToday)
-console.log(clientSpentForAllTime)
-
-// clientName = clientName.trim()
+clientName = clientName.trim()
 clientSpentToday = clientSpentToday.trim()
 clientSpentToday = Number(clientSpentToday)
 clientSpentForAllTime = clientSpentForAllTime.trim()
 clientSpentForAllTime = Number(clientSpentForAllTime)
 
-console.log(typeof (clientSpentToday))
-console.log(typeof (clientSpentForAllTime))
-console.log(clientSpentToday)
-console.log(clientSpentForAllTime)
+let examClientSpentToday = typeof (clientSpentToday) === 'number' && clientSpentToday // num and !NaN
+let examClientSpentForAllTime = typeof (clientSpentForAllTime) === 'number' && clientSpentForAllTime
 
-let wer = typeof (clientSpentToday) === 'number' && clientSpentToday
-let wur = typeof (clientSpentForAllTime) === 'number' && clientSpentForAllTime
+if (examClientSpentToday && examClientSpentForAllTime) {
+	let discount = 0
+	if (clientSpentForAllTime >= 100 && clientSpentForAllTime < 300) {
+		discount = 10
+	} else if (clientSpentForAllTime >= 300 && clientSpentForAllTime < 500) {
+		discount = 20
+	} else if (clientSpentForAllTime >= 500) {
+		discount = 30
+	}
 
-console.log(wer, wur)
+	alert(`You are given a discount in ${discount}%!`)
 
-if (wer && wur) {
-	console.log('ok')
-} else { console.log('false') }
+	clientSpentForAllTime += clientSpentToday - (clientSpentToday / 100 * discount)
 
-
-// else if (typeof (clientSpentForAllTime) != Number) {
-// 	alert('Сумма, которую клиент потратил за сегодня, должна быть числом! Перезагрузите страницу, чтобы повторить попытку.')
-// }
-
-
-// let discount = 0
-// if (clientSpentForAllTime >= 100 && clientSpentForAllTime < 300) {
-// 	discount = 10
-// } else if (clientSpentForAllTime >= 300 && clientSpentForAllTime < 500) {
-// 	discount = 20
-// } else if (lientSpentForAllTime >= 500) {
-// 	discount = 30
-// }
-
-// alert(`You are given a discount in ${discount}%!`)
-
-// clientSpentForAllTime += clientSpentToday - (clientSpentToday / 100 * discount)
-
-// alert(`Thanks, ${clientName}! You will pay ${clientSpentToday - (clientSpentToday / 100 * discount)}$. 
-// For all the time in our restaurant you spent ${clientSpentForAllTime}$.`)
+	alert(`Thanks, ${clientName}! You will pay ${clientSpentToday - (clientSpentToday / 100 * discount)}$. 
+For all the time in our restaurant you spent ${clientSpentForAllTime}$.`)
+} else { alert('The amount that the client spent for all the time and which was spent today should be a number! Reload the page to repeat the attempt.') }
