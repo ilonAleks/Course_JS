7526ff0aa30eb1d74c14b1045c4b514753317e5b
ilonaleks <rogilona@gmail.com>
2022-09-25 19:13:44 +0300

    9.end

--- 9.Arrays/9-Additional-1/9-Additional-1.js
+++ 9.Arrays/9-Additional-1/9-Additional-1.js
@@ -1,38 +1,4 @@
-//! const array = ["100", "hello", "javascript", , "help200", "+", 4]
-//! let toNum = Number(array[4])
-//! console.log(toNum)
-//! console.log(typeof (toNum))
-
 const getMathResult = function (expression) {
-
-	// const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-	// 	'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-	// if (expression.length > 3) {
-	// 	for (let i = 0; i < expression.length; i++) {
-	// 		let el = expression[i]
-	// 		// console.log(el)
-	// 		if (el.search(letters) || el === empty) {
-	// 			let exp = expression.splice([i], 1)
-	// 		}
-	// 		console.log(expression)
-	// 	}
-	// 	console.log(expression)
-	// }
-
-
-	// 	if (expression.length > 3) {
-	// 		for (let i = 0; i < expression.length; i++) {
-	// 			let toNum = Number(expression[i])
-	// 			if (typeof (toNum) === number || toNum === undefined) {
-	// 				return expression.splice([i], 1)
-	// 			}
-	// 			console.log(expression)
-	// 		}
-	// 	}
-	// 	console.log(expression)
-	// }
-
-
 	const mathSimbol = ['>', '<', '=', '+', '-', '*', '/']
 	let firstElement = Number(expression[0])
 	let secondElement = Number(expression[2])
