0523a876d2e73775335e5f848f63bc8e410f56f2
IlonAleks <rogilona@gmail.com>
2022-09-28 18:09:32 +0300

    10-add-1_in_process

--- 10.Objects/10-Additional-1/10-Additional-1.js
+++ 10.Objects/10-Additional-1/10-Additional-1.js
@@ -1,8 +1,3 @@
-function getTotalPriceOfShoppingBag(shoppingBag) {
-
-	return
-}
-
 const groceries = {
 	"Orange Juice": {
 		price: 1.5,
@@ -14,11 +9,35 @@ const groceries = {
 	},
 	// more items...
 }
+let groceriesArray = Object.entries(groceries)
+console.log(groceriesArray)
+// [
+// ['Orange Juice', {price: 1.5, discount: 10}]
+// ['Chocolate', {price: 2, discount: 0}]
+// ]
 
-const shoppingBag = [
-	{ product: 'Chocolate', quantity: 3 },
-	{ product: 'Orange Juice', quantity: 23 },
-]
+function getTotalPriceOfShoppingBag(shoppingBag) {
+	const result = []
+	console.log(groceries.length)
+	for (let i = 0; i < shoppingBag.length; i++) {
+		for (let j = 0; j < groceriesArray.length; j++) {
+			for (let k = 0; k < groceriesArray[j].length; j++) {
+				if (shoppingBag[i].product === groceriesArray[j][k]) {
+					result.push({
+						product: groceriesArray[j][k]
+					})
+				}
+			}
+		}
+		// return
+	}
 
-const totalPrice = getTotalPriceOfShoppingBag(shoppingBag);
-console.log('totalPrice', totalPrice); // Возвращает 37.05
+
+
+	const shoppingBag = [
+		{ product: 'Chocolate', quantity: 3 },
+		{ product: 'Orange Juice', quantity: 23 },
+	]
+
+	const totalPrice = getTotalPriceOfShoppingBag(shoppingBag);
+	console.log('totalPrice', totalPrice); // Возвращает 37.05
