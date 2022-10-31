53890378cd259c2f73434a714527d7ae5a6c7f8e
IlonAleks <rogilona@gmail.com>
2022-10-31 18:05:46 +0300

    fast-company 9.3_completed

--- React/fast-company/src/layouts/login.jsx
+++ React/fast-company/src/layouts/login.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
-    return <h1>Login</h1>;
+    const [data, setData] = useState({ email: "", password: "" });
+
+    const handleChange = ({ target }) => {
+        setData((prevState) => ({
+            ...prevState,
+            [target.name]: target.value
+        }));
+    };
+    return (
+        <form action="">
+            <div>
+                <label htmlFor="email">Email</label>
+                <input
+                    type="text"
+                    id="email"
+                    name="email"
+                    value={data.email}
+                    onChange={handleChange}
+                />
+            </div>
+            <div>
+                <label htmlFor="password">Password</label>
+                <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    value={data.password}
+                    onChange={handleChange}
+                />
+            </div>
+        </form>
+    );
 };
 
 export default Login;
