040307f04006ce92764f60b7321243511a806845
IlonAleks <rogilona@gmail.com>
2022-12-14 16:52:45 +0300

    Fast-Company 16.3

--- React/13_fast-company/src/app/hooks/useAuth.jsx
+++ React/13_fast-company/src/app/hooks/useAuth.jsx
@@ -74,7 +74,9 @@ const AuthProvider = ({ children }) => {
             }
         }
     }
-
+    function randomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
     async function singUp({ email, password, ...rest }) {
         try {
             const { data } = await httpAuth.post(`accounts:signUp`, {
@@ -83,7 +85,13 @@ const AuthProvider = ({ children }) => {
                 returnSecureToken: true
             });
             setTokens(data);
-            await createUser({ _id: data.localId, email, ...rest });
+            await createUser({
+                _id: data.localId,
+                email,
+                rate: randomInt(1, 5),
+                completedMeetings: randomInt(0, 300),
+                ...rest
+            });
         } catch (error) {
             errorCatcher(error);
             const { code, message } = error.response.data.error;
