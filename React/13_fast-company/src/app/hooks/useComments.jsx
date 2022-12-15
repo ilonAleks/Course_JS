f04d29e2fcc711672b7aa5877e5517d74966ee1c
IlonAleks <rogilona@gmail.com>
2022-12-15 11:35:45 +0300

    Fast-Company 16.20

--- React/13_fast-company/src/app/hooks/useComments.jsx
+++ React/13_fast-company/src/app/hooks/useComments.jsx
@@ -1,6 +1,9 @@
 import React, { useContext, useEffect, useState } from "react";
 import PropTypes from "prop-types";
+import { useParams } from "react-router-dom";
+import { useAuth } from "./useAuth";
 // import { toast } from "react-toastify";
+import { nanoid } from "nanoid";
 
 const CommentsContext = React.createContext();
 
@@ -12,12 +15,25 @@ export const CommentsProvider = ({ children }) => {
     // const [isLoading, setIsLoading] = useState(true);
     const [comments, setComments] = useState([]);
     // const [error, setError] = useState(null);
+    const { userId } = useParams();
+    const { currentUser } = useAuth();
 
     useEffect(() => {
         setComments(null);
     }, []);
+
+    async function createComment(data) {
+        const comment = {
+            ...data,
+            _id: nanoid(),
+            pageId: userId,
+            created_at: Date.now(),
+            userId: currentUser._id
+        };
+        console.log(comment);
+    }
     return (
-        <CommentsContext.Provider value={{ comments }}>
+        <CommentsContext.Provider value={{ comments, createComment }}>
             {children}
         </CommentsContext.Provider>
     );
