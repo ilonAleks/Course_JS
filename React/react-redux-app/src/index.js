96f5f81a4394547c5287ecf8b2f5d40edfed4baf
ilonaleks <rogilona@gmail.com>
2022-12-18 23:19:10 +0300

    redux 17.12

--- React/react-redux-app/src/index.js
+++ React/react-redux-app/src/index.js
@@ -8,14 +8,39 @@ function createStore(initialState) {
     return state;
   }
 
-  return { getState };
+  function dispatch(action) {
+    console.log(action);
+    if (action.type === "task/completed") {
+      const newArray = [...state];
+      const elementIndex = newArray.findIndex(
+        (el) => el.id === action.payload.id
+      );
+      newArray[elementIndex].completed = true;
+      state = newArray;
+      console.log(state);
+    }
+  }
+
+  return { getState, dispatch };
 }
 
 const store = createStore([{ id: 1, description: "Task 1", completed: false }]);
 
 const App = () => {
   console.log(store.getState());
-  return <h1>App</h1>;
+
+  return (
+    <>
+      <h1>App</h1>
+      <button
+        onClick={() =>
+          store.dispatch({ type: "task/completed", payload: { id: 1 } })
+        }
+      >
+        Complete
+      </button>
+    </>
+  );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
