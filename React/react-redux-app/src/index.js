f10825efe688903366490a3f4d861a08250f6876
IlonAleks <rogilona@gmail.com>
2022-12-19 14:21:02 +0300

    redux 17.20

--- React/react-redux-app/src/index.js
+++ React/react-redux-app/src/index.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
-import { initiateStore } from "./store/store";
-import * as actions from "./store/actions";
+import configureStore from "./store/store";
+import * as actions from "./store/task/actions";
 
-const store = initiateStore();
+const store = configureStore();
 
 const App = () => {
   const [state, setState] = useState(store.getState());
--- React/react-redux-app/src/store/store.js
+++ React/react-redux-app/src/store/store.js
@@ -1,11 +1,13 @@
 import { legacy_createStore } from "redux";
-import { taskReducer } from "./taskReducer";
+import { taskReducer } from "./task/taskReducer";
 
 const initialState = [
   { id: 1, title: "Task 1", completed: false },
   { id: 2, title: "Task 2", completed: false },
 ];
 
-export function initiateStore() {
+function configureStore() {
   return legacy_createStore(taskReducer, initialState);
 }
+
+export default configureStore;
