4d4e6003f7808c29bde3629920872744750497a4
IlonAleks <rogilona@gmail.com>
2022-12-19 15:41:33 +0300

    redux 17.25

--- React/react-redux-app/src/store/store.js
+++ React/react-redux-app/src/store/store.js
@@ -2,7 +2,10 @@ import { legacy_createStore } from "redux";
 import taskReducer from "./task";
 
 function configureStore() {
-  return legacy_createStore(taskReducer);
+  return legacy_createStore(
+    taskReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
 }
 
 export default configureStore;
