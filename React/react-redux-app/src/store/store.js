5ffad6b9f94c400e5b54154a2f9172f6d95ac4cf
IlonAleks <rogilona@gmail.com>
2022-12-19 18:17:50 +0300

    redux 17.32

--- React/react-redux-app/src/store/store.js
+++ React/react-redux-app/src/store/store.js
@@ -1,19 +1,13 @@
-import { legacy_createStore, compose, applyMiddleware } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import { logger } from "./middleware/loger";
-import { thunk } from "./middleware/thunk";
 import taskReducer from "./task";
 
-const middlewareEnhancer = applyMiddleware(logger, thunk);
-
-function configureStore() {
-  return legacy_createStore(
-    taskReducer,
-    compose(
-      middlewareEnhancer,
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-  );
+function createStore() {
+  return configureStore({
+    reducer: taskReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+    devTools: process.env.NODE_ENV !== "production",
+  });
 }
 
-export default configureStore;
+export default createStore;
