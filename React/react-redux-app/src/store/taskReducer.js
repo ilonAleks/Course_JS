5f727da4c360b6c415b1ab98c4127969ba8e05a1
IlonAleks <rogilona@gmail.com>
2022-12-19 13:35:44 +0300

    redux 17.16

---
+++ React/react-redux-app/src/store/taskReducer.js
@@ -0,0 +1,18 @@
+import { taskUpdated } from "./actionTypes";
+
+export function taskReducer(state, action) {
+  switch (action.type) {
+    case taskUpdated: {
+      const newArray = [...state];
+      const elementIndex = newArray.findIndex(
+        (el) => el.id === action.payload.id
+      );
+
+      newArray[elementIndex] = { ...newArray[elementIndex], ...action.payload };
+      return newArray;
+    }
+
+    default:
+      break;
+  }
+}
