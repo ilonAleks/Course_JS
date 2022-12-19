7da4dca2ae5d98092c97ace9040557b5c00fa64f
IlonAleks <rogilona@gmail.com>
2022-12-19 15:27:21 +0300

    redux 17.23

--- React/react-redux-app/src/store/store.js
+++ React/react-redux-app/src/store/store.js
@@ -1,13 +1,8 @@
 import { legacy_createStore } from "redux";
 import taskReducer from "./task";
 
-const initialState = [
-  { id: 1, title: "Task 1", completed: false },
-  { id: 2, title: "Task 2", completed: false },
-];
-
 function configureStore() {
-  return legacy_createStore(taskReducer, initialState);
+  return legacy_createStore(taskReducer);
 }
 
 export default configureStore;
--- React/react-redux-app/src/store/task.js
+++ React/react-redux-app/src/store/task.js
@@ -1,7 +1,12 @@
-import { createAction } from "@reduxjs/toolkit";
+import { createAction, createReducer } from "@reduxjs/toolkit";
 
 const update = createAction("task/updated");
 const remove = createAction("task/removed");
+
+const initialState = [
+  { id: 1, title: "Task 1", completed: false },
+  { id: 2, title: "Task 2", completed: false },
+];
 
 export function taskCompleted(id) {
   return update({ id, completed: true });
@@ -15,24 +20,16 @@ export function taskDeleted(id) {
   return remove({ id });
 }
 
-function taskReducer(state = [], action) {
-  switch (action.type) {
-    case update.type: {
-      const newArray = [...state];
-      const elementIndex = newArray.findIndex(
-        (el) => el.id === action.payload.id
-      );
+const taskReducer = createReducer(initialState, (builder) => {
+  builder
+    .addCase(update, (state, action) => {
+      const elementIndex = state.findIndex((el) => el.id === action.payload.id);
 
-      newArray[elementIndex] = { ...newArray[elementIndex], ...action.payload };
-      return newArray;
-    }
-    case remove.type: {
+      state[elementIndex] = { ...state[elementIndex], ...action.payload };
+    })
+    .addCase(remove, (state, action) => {
       return state.filter((el) => el.id !== action.payload.id);
-    }
-
-    default:
-      return state;
-  }
-}
+    });
+});
 
 export default taskReducer;
