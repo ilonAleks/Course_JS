2633742324a5681b974dd10e743cfd0a3060e48e
IlonAleks <rogilona@gmail.com>
2022-12-20 15:48:37 +0300

    fast-company 18.10

--- React/13_fast-company/src/app/store/users.js
+++ React/13_fast-company/src/app/store/users.js
@@ -2,6 +2,7 @@ import { createAction, createSlice } from "@reduxjs/toolkit";
 import authService from "../service/authService";
 import localStorageService from "../service/localStorageService";
 import userService from "../service/userService";
+import randomInt from "../utils/randomInt";
 
 const usersSlice = createSlice({
     name: "users",
@@ -29,6 +30,9 @@ const usersSlice = createSlice({
         },
         authRequesFaild: (state, action) => {
             state.error = action.payload;
+        },
+        userCreated: (state, action) => {
+            state.entities.push(action.payload);
         }
     }
 });
@@ -39,10 +43,14 @@ const {
     usersReceved,
     usersRequestFailed,
     authRequesSuccess,
-    authRequesFaild
+    authRequesFaild,
+    userCreated
 } = actions;
 
 const authRequested = createAction("users/authRequested");
+const userCreateRequested = createAction("users/userCreateRequested");
+const createUserFaild = createAction("users/createUserFaild");
+
 export const singUp =
     ({ email, password, ...rest }) =>
     async (dispatch) => {
@@ -51,10 +59,36 @@ export const singUp =
             const data = await authService.register({ email, password });
             localStorageService.setTokens(data);
             dispatch(authRequesSuccess({ userId: data.localId }));
+            dispatch(
+                createUser({
+                    _id: data.localId,
+                    email,
+                    rate: randomInt(1, 5),
+                    completedMeetings: randomInt(0, 200),
+                    image: `https://avatars.dicebear.com/api/avataaars/${(
+                        Math.random() + 1
+                    )
+                        .toString(36)
+                        .substring(7)}.svg`,
+                    ...rest
+                })
+            );
         } catch (error) {
             dispatch(authRequesFaild(error.message));
         }
     };
+
+function createUser(payload) {
+    return async function (dispatch) {
+        dispatch(userCreateRequested());
+        try {
+            const { content } = await userService.create(payload);
+            dispatch(userCreated(content));
+        } catch (error) {
+            dispatch(createUserFaild(error.message));
+        }
+    };
+}
 
 export const loadUsersList = () => async (dispatch) => {
     dispatch(usersRequested());
