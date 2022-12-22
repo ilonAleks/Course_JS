4e0448f8b864d1ae97eeba8c52902a602d0ba7ab
IlonAleks <rogilona@gmail.com>
2022-12-22 17:00:51 +0300

    Node_modul_1 2.6

--- Node/node_modul_1/index.js
+++ Node/node_modul_1/index.js
@@ -18,8 +18,8 @@ yargs.command({
 yargs.command({
   command: "list",
   describe: "Print all notes",
-  handler() {
-    const notes = getNotes();
+  async handler() {
+    const notes = await getNotes();
     console.log(notes);
   },
 });
--- Node/node_modul_1/notes.controller.js
+++ Node/node_modul_1/notes.controller.js
@@ -1,7 +1,23 @@
 const fs = require("fs/promises");
+const path = require("path");
+
+const notesPath = path.join(__dirname, "db.json");
+//! node notes.controller => C:\Users\i.roguleva\лич\Course_JS\Node\node_modul_1\db.json
 
 async function addNote(title) {
-  const notes = require("./db.json");
+  // const buffer = await fs.readFile(notesPath);
+  // console.log(buffer); //!<Buffer 5b 7b 22 74 69.... 35 more butes>
+  // const notes = Buffer.from(buffer).toString("utf-8");
+  // console.log(notes); //! [{"title":"Hello","id":"1671716025036"},{"title":"From-console","id":"1671716118381"}]
+  // console.log(typeof notes); //!string
+  // console.log(typeof JSON.stringify(notes)); //!object
+
+  const notes = await getNotes();
+  // console.log(notes);
+  //! [
+  //!   {"title":"Hello","id":"1671716025036"},
+  //!   {"title":"From-console","id":"1671716118381"}
+  //! ]
 
   const note = {
     title,
@@ -9,26 +25,18 @@ async function addNote(title) {
   };
 
   notes.push(note);
-  await fs.writeFile("./db.json", JSON.stringify(notes));
+  await fs.writeFile(notesPath, JSON.stringify(notes));
 }
 
-function getNotes() {
-  return require("./db.json");
+async function getNotes() {
+  const notes = await fs.readFile(notesPath, { encoding: "utf-8" });
+  return Array.isArray(JSON.parse(notes)) ? JSON.parse(notes) : [];
+  // если массив, то возвращаем массив, если нет - пустой массив
 }
+
+addNote("Test");
 
 module.exports = {
   addNote,
   getNotes,
 };
-
-// node index.js add --title=Hello => запись в db.json
-// [{ "title": "Hello", "id": "1671716025036" }]
-
-// node index.js add --title=From-console => запись в db.json
-// [{"title":"Hello","id":"1671716025036"},{"title":"From-console","id":"1671716118381"}]
-
-//node index.js list =>
-// [
-//   { title: 'Hello', id: '1671716025036' },
-//   { title: 'From-console', id: '1671716118381' }
-// ]
