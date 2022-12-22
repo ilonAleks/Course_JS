bb823a954ed739a0f9758da0bce512fb4a675b20
IlonAleks <rogilona@gmail.com>
2022-12-22 15:57:14 +0300

    Node_modul_1 2.2

---
+++ Node/node_modul_1/index.js
@@ -0,0 +1,17 @@
+// require("./module");
+
+// const person = {
+//   name: "Ilona",
+//   age: 33,
+// };
+
+// function getName(p) {
+//   return p.name;
+// }
+
+// console.log(getName(person));
+
+// console.log(__filename);
+// console.log(__dirname);
+
+console.log(process.argv);
