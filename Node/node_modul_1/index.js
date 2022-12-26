6f589a7d751dbf736a6bd444aaac5439266c75a3
IlonAleks <rogilona@gmail.com>
2022-12-26 18:12:23 +0300

    Node_modul_1 4.3

--- Node/node_modul_1/index.js
+++ Node/node_modul_1/index.js
@@ -1,13 +1,20 @@
 const http = require("http");
 const chalk = require("chalk");
+const fs = require("fs/promises");
+const path = require("path");
 
 const port = 3000;
+const basePath = path.join(__dirname, "pages");
 
-const server = http.createServer((req, res) => {
-  console.log("Request metod:", req.method);
-  console.log("Request url:", req.url);
-
-  res.end("Hello from server!!!");
+const server = http.createServer(async (req, res) => {
+  if (req.method === "GET") {
+    const content = await fs.readFile(path.join(basePath, "index.html"));
+    // res.setHeader("Content-Type", "text/html");
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+    });
+    res.end(content);
+  }
 });
 
 server.listen(port, () => {
