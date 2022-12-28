cd9281889a9ca03762be28d06b2e46683e6b3a52
IlonAleks <rogilona@gmail.com>
2022-12-28 13:49:07 +0300

    React-app 5.8

--- Node/react-app/server/app.js
+++ Node/react-app/server/app.js
@@ -2,14 +2,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const config = require("config");
 const chalk = require("chalk");
-
-// Текущий IP-адрес 138.199.7.187/32
+const initDatabase = require("./startUp/initDatabase.js");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-mongoose.set("strictQuery", false);
 
 const PORT = config.get("port") ?? 8080;
 
@@ -21,7 +19,11 @@ const PORT = config.get("port") ?? 8080;
 
 async function start() {
   try {
+    mongoose.connection.once("open", () => {
+      initDatabase();
+    });
     await mongoose.connect(config.get("mongoUri"));
+    console.log(chalk.green("MongoDB connected"));
     app.listen(PORT, () => {
       console.log(chalk.green("Server has been started"));
     });
