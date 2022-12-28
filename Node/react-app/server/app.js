279ee569f24298272f1caf79ab3483292b70b6e3
IlonAleks <rogilona@gmail.com>
2022-12-28 13:19:28 +0300

    React-app 5.7

--- Node/react-app/server/app.js
+++ Node/react-app/server/app.js
@@ -3,10 +3,13 @@ const mongoose = require("mongoose");
 const config = require("config");
 const chalk = require("chalk");
 
+// Текущий IP-адрес 138.199.7.187/32
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+mongoose.set("strictQuery", false);
 
 const PORT = config.get("port") ?? 8080;
 
