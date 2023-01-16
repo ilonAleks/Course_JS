1e07a27eaea7c283f3d26d06bfa287421349765e
IlonAleks <rogilona@gmail.com>
2023-01-16 19:25:01 +0300

    React-app 7.3

--- Node/react-app/server/routes/comment.routes.js
+++ Node/react-app/server/routes/comment.routes.js
@@ -1,4 +1,58 @@
+//| URL 								| Auth | Type	 	| Body 		| Query 						| Return 		|
+//| /api/comment 				| true | get 		| null 		| orderBy, equalTo 	| Comment[] |
+//| /api/comment 				| true | post 	| Comment | null 							| Comment 	|
+//| /api/comment/:id	 	| true | delete | null 		| null 							| null 			|
+
 const express = require("express");
+const auth = require("../middleware/auth.middleware");
+const Comment = require("../models/Comment");
 const router = express.Router({ mergeParams: true });
+
+// /api/comment
+router
+  .route("/")
+  .get(auth, async (req, res) => {
+    try {
+      const { orderBy, equalTo } = req.query;
+      const list = await Comment.find({ [orderBy]: equalTo });
+      res.send(list);
+    } catch (e) {
+      res
+        .status(500)
+        .json({ message: "An error occurred on the server.try later" });
+    }
+  })
+  .post(auth, async (req, res) => {
+    try {
+      const newComment = await Comment.create({
+        ...req.body,
+        userId: req.user._id,
+      });
+      res.status(201).send(newComment);
+    } catch (e) {
+      res
+        .status(500)
+        .json({ message: "An error occurred on the server.try later" });
+    }
+  });
+
+router.delete("/:commentId", auth, async (req, res) => {
+  try {
+    const { commentId } = req.params;
+    const removedComment = await Comment.findById(commentId);
+    // OR const removedComment = await Comment.find({ _id: commentId });
+
+    if (removedComment.userId.toString() === req.user._id) {
+      await removedComment.remove();
+      return res.send(null);
+    } else {
+      res.status(401).json({ message: "Unauthorized" });
+    }
+  } catch (e) {
+    res
+      .status(500)
+      .json({ message: "An error occurred on the server.try later" });
+  }
+});
 
 module.exports = router;
