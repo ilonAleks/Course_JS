2f0452ae612a70b7d6419a7c5034cdd490972625
IlonAleks <rogilona@gmail.com>
2023-01-19 17:36:41 +0300

    React-app 8.2

--- Node/react-app/server/app.js
+++ Node/react-app/server/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const config = require("config");
 const chalk = require("chalk");
+const cors = require("cors");
 const initDatabase = require("./startUp/initDatabase.js");
 const routes = require("./routes");
 
@@ -9,6 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 app.get("/", function (req, res, next) {
   res.send("Hello world");
