1ab791203fd9b7db37f70cff467ccebceeefb623
IlonAleks <rogilona@gmail.com>
2023-01-19 18:50:57 +0300

    React-app 8.5 error

--- Node/react-app/client/src/app/store/users.js
+++ Node/react-app/client/src/app/store/users.js
@@ -87,8 +87,8 @@ export const login =
         dispatch(authRequested());
         try {
             const data = await authService.login({ email, password });
-            dispatch(authRequestSuccess({ userId: data.localId }));
             localStorageService.setTokens(data);
+            dispatch(authRequestSuccess({ userId: data.userId }));
             history.push(redirect);
         } catch (error) {
             const { code, message } = error.response.data.error;
